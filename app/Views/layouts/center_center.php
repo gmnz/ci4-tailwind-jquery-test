380415352cee09b61a3f3bca0a715c762f6d1241
Michael Kristof <mich4el.kristof@gmail.com>
2022-07-14 19:55:31 +0200

    back to dashboard link

--- app/Views/layouts/center_center.php
+++ app/Views/layouts/center_center.php
@@ -9,6 +9,15 @@
         <div class="flex justify-center h-full">
             <div class="grid grid-cols-1 gap-3 content-center text-center h-full">
                 <?= $this->renderSection('body') ?>
+                <div class="mt-5">
+                    <?php
+                        if(!strpos(current_url(), "/dashboard")) {
+                    ?>
+                            <a href="/dashboard">Go Back to Dashboard</a>
+                    <?php
+                        }
+                    ?>
+                </div>
             </div>
         </div>
     </div>
