0177ba509da0392648af1e76f1ee6164392592c8
Michael Kristof <mich4el.kristof@gmail.com>
2022-07-17 12:02:06 +0200

    look changes

--- app/Views/userProfile/index.php
+++ app/Views/userProfile/index.php
@@ -2,14 +2,22 @@
 
 <?= $this->section('body') ?>
         <table class="m-5">
+            <tr>
+                <th class="border border-black-3 p-2">ID</th>
+                <th class="border border-black-3 p-2">Username</th>
+                <th class="border border-black-3 p-2">E-mail</th>
+            </tr>
             <?php
                 foreach($userData as $user) {
             ?>
                     <tr>
-                        <td>
+                        <td class="border border-black-3 p-2">
+                            <?= $user['user_id'] ?>
+                        </td>
+                        <td class="border border-black-3 p-2">
                             <?= $user['name'] ?>
                         </td>
-                        <td>
+                        <td class="border border-black-3 p-2"> 
                             <?= $user['email'] ?>
                         </td>
                     </tr>
